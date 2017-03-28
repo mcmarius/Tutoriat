e16726be29e6e023717f95070a3be0b55864698e
mcmarius <m.c.marius@outlook.com>
2017-03-28 18:18:58 +0300

    Update fisiere main

---
+++ include.h
@@ -0,0 +1,13 @@
+//
+// Created by marius on 14/03/2017.
+//
+
+#ifndef TUTORIAT_INCLUDE_H
+#define TUTORIAT_INCLUDE_H
+
+#include <iostream>
+#include <cstring>
+
+using std::cout;
+
+#endif //TUTORIAT_INCLUDE_H
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "include.h"
+#include "Pacient.h"
+
+int main() {
+    Pacient pacient("Paun", "Andrei", "academiei 14", 45);
+    cout<<pacient;
+    return 0;
+}
